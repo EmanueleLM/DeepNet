1a2b3ec8fb3e771a0bea4e4f3632baba98aae85e
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-09-23 23:21:29 +0200

    added loss dictionary support to call loss funcs

--- deepnet.py
+++ deepnet.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 23 12:59:16 2017 
+Created on Sat Sep 23 12:59:16 2017
 
 @author: Emanuele
 
@@ -11,6 +11,7 @@ The starting point is just a feed forward neural net where we add much more laye
 
 import numpy as np
 import activations as act
+import loss as ls
 import pylab as pl
 import time
 
@@ -22,19 +23,28 @@ import time
 #   call the function in this way activation_dictionary["name_to_invoke_the_function"](input)
 activations_dict = {"relu": act.relu, "sigmoid": act.sigma, "tanh": act.tanh};
 
+# the same method is employed for the choice of the loss function
+# Use this struct in this way: 
+#   define a function in loss.py (imported let's say as ls), say foo(input)
+#   put in the vocabulary the record "name_to_invoke_the_function": ls.function_name
+#   call the function in this way activation_dictionary["name_to_invoke_the_function"](input)
+loss_dict = {"L1": ls.lossL1, "L2":ls.lossL2, "Llog": ls.lossLog};
+
 # =============================================================================
 #  class that models a deep network with multiple layers and different acrivation functions
 #  take as input (the __init__ function):
 #      input_size: size of a generic input sample
 #      layers: a numpy.array (not a list since we will use numpy.size on it) that contains a number and a string: the former specifies the number of neurons on that layer
 #          the latter specifies the activation function (e.g. a valid layers is list([[1,"relu"][5, "sigmoid"]]))
+#      loss: the loss function selected for the backpropagation phase
 # =============================================================================
 class DeepNet(object):
-    def __init__(self, input_size, layers):
+    def __init__(self, input_size, layers, loss):
         self.W = list(); # list that contains all the weights in the net (we use a list and np.array for each matrix of weights, for efficiency reasons)
         self.W.append(np.array(np.zeros([input_size, np.int(layers[0][0])])));
         self.Bias = np.zeros([layers.shape[0], 1]);
         self.activations = np.array(layers[:,1]);
+        self.loss = loss;
         for l in range(len(layers)-1):
             self.W.append(np.array(np.zeros([np.int(layers[l][0]), np.int(layers[l+1][0])])));
         print("\nNetwork created succesfully!")
@@ -46,8 +56,8 @@ class DeepNet(object):
         print("\nThe network has ", self.W[0].shape[0], " input(s) and ", len(self.W), " layers.");
         for l in range(len(self.W)):
             print("The layer number ", l+1, " has ", self.W[l].shape[0], " input(s) per neuron, ", self.W[l].shape[1], " neuron(s) and ", self.activations[l], " as activation function.");
-         
-    # function that activates a given layer and based on its activation function, returns the desired output
+            print("The loss function selected is ", self.loss);
+    # function that activates a single, given layer and, based on its activation function, returns the desired output
     # takes as input
     #   the input X as a vector
     #   the layer where we want the activation to happen
@@ -57,7 +67,7 @@ class DeepNet(object):
         Z = np.dot(self.W[layer].T,X)+self.Bias[layer]; # activate (linearly) the input
         return activations_dict[self.activations[layer]](Z); # activate the actiovation function of each layer using the vocabulary defined at the beginning            
     
-    # function that activates a all the layers in the net and returns the desired output
+    # function that activates sequentially all the layers in the net and returns the desired output (a.k.a. "forward")
     # takes as input
     #   the input X as a vector
     # returns
@@ -67,7 +77,16 @@ class DeepNet(object):
         for l in range(len(self.W)):
             res = self.activation(res, l);
         return res; 
+    # function that calculates the loss for a given input, output and a loss function
+    # takes as input:
+    #   the prediction Y (can be calculated as self.netActivation(input))
+    #   the target value 
+    # returns:
+    #   the loss with the formula specified in self.loss, using the dictionary structure loss_dict to invoke the correct function (see above for more info)
+    def calculateLoss(self, Y, T):
+        return loss_dict[self.loss](Y, T)
+    
     
 """ Test part """
 
-net = DeepNet(2, np.array([[3, "relu"],[5, "relu"], [7, "relu"], [1, "sigmoid"]]));
+net = DeepNet(2, np.array([[3, "relu"],[5, "relu"], [7, "relu"], [1, "sigmoid"]]), "L2");
