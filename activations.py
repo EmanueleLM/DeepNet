4a2785ab7557af27bc0c63b34863ce3f75a50c57
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-09-23 20:52:40 +0200

    module that defines several activation functions

---
+++ activations.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 23 20:02:59 2017
+
+@author: Emanuele
+
+Activation functions, versions with single sample, with full dataset, both SIMD and batch.
+"""
+
+import numpy as np
+
+# logistic function activation
+def sigma(Z):
+    return 1/(1+np.exp(-Z));
+    
+# ReLU activation function
+def relu(Z):
+    Z[Z<=0] = 0;
+    return Z;
+
+# tanh activation function
+def tanh(Z):
+    return (np.exp(Z)-np.exp(-Z))/(np.exp(Z)+np.exp(-Z));
+
+def foo(f):
+    print(f);
