8534e06faeadb3978a127a56bd5e0af5133eb314
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-10-16 15:44:04 +0200

    early stopping implemented for the digit dataset

--- deepnet.py
+++ deepnet.py
@@ -61,7 +61,7 @@ class DeepNet(object):
         self.Bias = np.zeros([layers.shape[0], 1]);
         self.activations = np.array(layers[:,1]);
         self.loss = loss;
-        self.learning_rate = 0.08; # default learning rate for each iteration phase
+        self.learning_rate = 0.002; # default learning rate for each iteration phase
         self.dW_old = list(); # list that contains (usually) the weights of a past iteration: you may use it for the momentum's update
         self.dW_old.append(np.array(np.zeros([input_size, np.int(layers[0][0])])));
         self.dB_old = np.zeros([layers.shape[0], 1]);
@@ -211,7 +211,7 @@ class DeepNet(object):
         #print("Weights' updates", dW);
         #print("biases' updates", dB);
         # perform weights update self.W[i] = self.W[i] - l_rate[i]*dY*dW[i]
-        self.weightsUpdate(dW, dB);
+        self.weightsUpdateWithMomentum(dW, dB);
         return;
         
     # function that updates the weights of the net
@@ -242,7 +242,7 @@ class DeepNet(object):
 #Y = da.randomData(1000,10);
 #X = da.normalizeData(X); # normalize the input (except for the prediction labels)
 
-net = DeepNet(64, np.array([[275, "sigmoid"], [10, "sigmoid"]]), "L2");
+net = DeepNet(64, np.array([[128, "sigmoid"], [10, "sigmoid"]]), "L2");
 for i in range(len(net.W)):
     net.setWeights(weights_dict['lecun'](net.W[i]), i);
 #print("Initial weights ", net.W);
@@ -258,7 +258,8 @@ for i in range(len(net.W)):
 """ Test the net with a simple digit recognition test """
 import utils_digit_recognition as drec
 
-train_percentage = 90; # percentage of the dataset used for training
+train_percentage = 60; # percentage of the dataset used for training
+validation_percentage = 20; # this percentage must be lower than the test set, since it's taken directly from it (for the sake of simplicity)
 
 digits = drec.load_digits(); # import the dataset
 
@@ -269,8 +270,12 @@ images, targets = drec.unison_shuffled_copies(digits.images, digits.target); # s
 train, test = drec.dataSplit(images, train_percentage);# split train adn test
 train_Y, test_Y = drec.dataSplit(targets, train_percentage); # split train and test labels
 
+validation, test = drec.dataSplit(test, validation_percentage);
+validation_Y, test_Y = drec.dataSplit(test_Y, validation_percentage);
+
 train_Y = drec.binarization(train_Y); # binarize both the train and test labels
 test_Y = drec.binarization(test_Y); # ..
+validation_Y = drec.binarization(validation_Y); # ..
 
 
 X = train.reshape(train.shape[0], train.shape[1]*train.shape[2]).T;
@@ -282,15 +287,30 @@ X = drec.normalizeData(X);
 X_test = test.reshape(test.shape[0], test.shape[1]*test.shape[2]).T;
 Y_test = test_Y;
 
+X_validation = validation.reshape(validation.shape[0], validation.shape[1]*validation.shape[2]).T;
+Y_validation = validation_Y;
+
+
 #X_test = drec.normalizeData(X_test);
 #Y_test = drec.normalizeData(Y_test.T).T;
 
 """ Train with full batch (size of the batch equals to the size of the dataset) """
-epochs = 5;
+epochs = 100;
+validation_error = 1; # validation stop metric, initially the error is everywhere
+validation_size = X_validation.shape[1];
 for e in range(epochs):
     print((epochs-e)," epochs left");
     for n in range(X.shape[1]):
         net.backpropagation(X[:,n].reshape(64,1), Y[n].reshape(10,1));
+        number_of_errors_validation = 0;
+    for n in range(X_validation.shape[1]):
+        if np.argmax(net.netActivation(X_validation[:,n].reshape(64,1))) != np.argmax(Y_validation[n].reshape(10,1)):
+            number_of_errors_validation += 1;
+    if float(number_of_errors_validation/validation_size) > validation_error:
+        break;
+    else:
+        validation_error = number_of_errors_validation/validation_size;
+        print("validation error: ", validation_error);
 
 """ test how much we are precise in our prediction """
 number_of_errors = 0; # total number of errors on the test set
