5b0e253d58b3f42f4cf0793db1f39708a0817756
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-10-20 13:38:33 +0200

    corrected errors with entropy and other stuff

--- loss.py
+++ loss.py
@@ -14,17 +14,22 @@ import numpy as np
 # Y is the value predicted by our algorithm
 # T is the real value (we are in a supervised scenario)
 def lossL2(Y, T):
-    return np.power((Y-T), 2);
+    return np.absolute(np.power((T-Y), 2));
 
 def lossL1(Y, T):
-    return np.abs((Y-T));
+    return np.abs((T-Y));
 
 # loss function known as Cross Entropy
 # Y is the value predicted by our algorithm
 # T is the real value (we are in a supervised scenario)
+# we expect that the number of dimensions is the first dimension of both the input (so Y.shape[1]=T.shape[1]=dimensions)
 def lossCrossEntropy(T, Y):
-    if Y==T: # this if else is to avoid numerical problems with numpy.log2(0)
-        return 0;
-    elif (Y==1 and T==0) or (Y==0 and T==1):
-        return 1;
-    return -T*np.log2(Y)-(1-T)*np.log2(1-Y);
+    res = np.zeros(Y.shape);
+    for i in range(Y.shape[0]):
+        if Y[i] == 0:
+           Y[i] = .0001;
+        elif Y[i]==1:
+            Y[i] = .9999;
+        else:
+            res[i] = -T[i]*np.log2(Y[i])-(1-T[i])*np.log2(1-Y[i]);
+    return res;
