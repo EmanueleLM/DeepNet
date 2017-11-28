a83d798e2173b0dcba63a194eb5b9586e8a7b71b
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-11-28 14:14:56 +0100

    minor updates to L1 loss and L2

--- derivatives.py
+++ derivatives.py
@@ -30,20 +30,13 @@ def dYL2(Y, T):
 
 # derivative of Y wrt the exit variable for the L1 loss function
 # which is basically the signum of the difference between T and Y (that's why in regression a lot of weights are zero)
-# please note that this one holds it returns a numpy array of size (2,1) since we have two different derivatives for each part
-#   (real, imaginary) of the loss, and each of them must be multiplied to different factors according the the chain rule
 def dYL1(Y, T):
-    if Y.dtype == 'complex' or Y.dtype == 'complex64':
-        dL_dYr = 2*(np.real(Y)-np.real(T)); # dLoss/dYr
-        dL_dYi = 2*(np.imag(Y)-np.imag(T)); # dLoss/dYi
-        return np.array([dL_dYr, dL_dYi]).reshape(2,1);
-    else:
-        return -np.sign(T-Y);
+    return -np.sign(T-Y);
     
 # derivative of Y wrt the exit variable for the Cross Entropy loss function  
 def dYCrossEntropy(Y, T):
     Y[Y==1] = 0.9999;
-    Y[Y==0] = 0.0001;
+    Y[Y==0] = 1e-4;
     return (Y-T)/(Y*(1-Y));
 
 # (partial) derivative of sigmoid function wrt variable Z
@@ -55,15 +48,15 @@ def dSigmoid(Z):
 # (partial) derivative of ReLu function wrt variable Z
 # the partial is not in the common math sense, but in the sense that the "residual" dZ is not calculated at this step
 def dRelu(Z):
-    Z[Z<=0] = 0;
-    Z[Z>0] = 1;
+    Z[Z<=0] = 0.;
+    Z[Z>0] = 1.;
     return Z;
 
 # (partial) derivative of LeakyReLu function wrt variable Z, where \epsilon is set to 0.01
 # the partial is not in the common math sense, but in the sense that the "residual" dZ is not calculated at this step
 def dLeakyRelu(Z):
     Z[Z<=0] = 0.1;
-    Z[Z>0] = 1;
+    Z[Z>0] = 1.;
     return Z;
 
 # (partial) derivative of tanh function wrt variable Z
