84d6c364fa6212048b932e6e7785414515a3b0a7
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-11-29 15:06:40 +0100

    net validation with 8x8 handwritten dataset

--- netgenerator.py
+++ netgenerator.py
@@ -66,12 +66,91 @@ def randomPopulation(input_size, output_size, population_size):
 #   a list of tuples <net, fitness>, where each net has its own fitness calculated
 def evaluateFitness(nets):
     population = list([[n, .0] for n in nets]); # put the couples <net, fitness>, initially fintess = 0 forall nets
+    num_parameters_per_net = np.array([n.numberOfParameters() for n in nets]); # extract for each net the number of parameters
+    max_num_parameters = np.max(num_parameters_per_net); # extract the size of the biggest net
     for n in range(len(nets)):
-        population[n][1] = .0; # calculate the fitness, this part should be massively parallelized
+        """ in this part we need to evaluate the fintess of a single network,
+            this means that we need to test the net against a problem and extract
+            the validation error. then we will calculate a measure of the size of the net
+            and we use those two parameters in a convex combination to calculate the 
+            fitness of each net """
+        population[n][1] = (1-validateNet(population[n][0])) + (1-(num_parameters_per_net[n]/max_num_parameters)); # calculate the fitness, this part should be massively parallelized
     return population;
+
+# function that validate the net i.e. calculate the validation error for a network
+# in this case we use the 8x8 handwritten dataset in scikit-learn
+#   we must implement a way to test it against a generic problem
+def validateNet(net):
+    for i in range(len(net.W)): #initialize the weights
+        net.setWeights(dn.weights_dict['lecun'](net.W[i]), i); 
+        
+    import utils_digit_recognition as drec
+    
+    train_percentage = 60; # percentage of the dataset used for training
+    validation_percentage = 20; # this percentage must be lower than the test set, since it's taken directly from it (for the sake of simplicity)
+    
+    digits = drec.load_digits(); # import the dataset
+    
+    train_size = len(digits.images); # train size is the number of samples in the digits' dataset
+    
+    images, targets = drec.unison_shuffled_copies(digits.images, digits.target); # shuffle together inputs and supervised outputs
+    
+    train, test = drec.dataSplit(images, train_percentage);# split train adn test
+    train_Y, test_Y = drec.dataSplit(targets, train_percentage); # split train and test labels
+    
+    validation, test = drec.dataSplit(test, validation_percentage);
+    validation_Y, test_Y = drec.dataSplit(test_Y, validation_percentage);
+    
+    train_Y = drec.binarization(train_Y); # binarize both the train and test labels
+    test_Y = drec.binarization(test_Y); # ..
+    validation_Y = drec.binarization(validation_Y); # ..
+    
+    
+    X = train.reshape(train.shape[0], train.shape[1]*train.shape[2]).T;
+    Y = train_Y;
+    
+    X = drec.normalizeData(X);
+    #Y = drec.normalizeData(Y.T).T;
+    
+    X_test = test.reshape(test.shape[0], test.shape[1]*test.shape[2]).T;
+    Y_test = test_Y;
+    
+    X_validation = validation.reshape(validation.shape[0], validation.shape[1]*validation.shape[2]).T;
+    Y_validation = validation_Y;
+    
+    
+    #X_test = drec.normalizeData(X_test);
+    #Y_test = drec.normalizeData(Y_test.T).T;
+    
+    """ Train with full batch (size of the batch equals to the size of the dataset) """
+    epochs = 3;
+    validation_error = 1; # validation stop metric, initially the error is everywhere
+    validation_size = X_validation.shape[1];
+    for e in range(epochs):
+        #print((epochs-e)," epochs left");
+        for n in range(X.shape[1]):
+            net.backpropagation(X[:,n].reshape(64,1), Y[n].reshape(10,1));
+            number_of_errors_validation = 0;
+        for n in range(X_validation.shape[1]):
+            if np.argmax(net.netActivation(X_validation[:,n].reshape(64,1))) != np.argmax(Y_validation[n].reshape(10,1)):
+                number_of_errors_validation += 1;
+        if float(number_of_errors_validation/validation_size) > validation_error:
+            break;
+        else:
+            validation_error = number_of_errors_validation/validation_size;
+            #print("validation error: ", validation_error);
+    
+    """ test how much we are precise in our prediction """
+    number_of_errors = 0; # total number of errors on the test set
+    test_size = X_test.shape[1];
+    for n in range(X_test.shape[1]):
+        if np.argmax(net.netActivation(X_test[:,n].reshape(64,1))) != np.argmax(Y_test[n].reshape(10,1)):
+            number_of_errors += 1;
+    #print("The error percentage is ", number_of_errors/test_size, ": ", number_of_errors," errors out of ", test_size, " samples on test set.");
+    return number_of_errors/test_size;
+    
         
 """ Test part """
-nets = randomPopulation(3, 10, 5); # create the population 
+nets = randomPopulation(64, 10, 2); # create the population 
 population = evaluateFitness(nets);  
 crossovers_dict["one-point"](nets[0], nets[1], 1); # apply crossover 
-print(selection_dict["rank"](population, False));
