04d3a13a89063cb69d3d1325bc6a2065a2ef8f91
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-12-06 14:03:21 +0100

    1-pt crossover with non fully conn. topologies

--- genetic/crossover.py
+++ genetic/crossover.py
@@ -37,6 +37,7 @@ def onePointCrossover(net1, net2, p):
             mask2 = cp.deepcopy(net2.mask); # copy the mask of the second net
             net1 = dn.DeepNet(net1.W[0].shape[0], layer1.reshape(int(layer1.shape[0]/2), 2), net2.loss, verbose=False);
             net2 = dn.DeepNet(net2.W[0].shape[0], layer2.reshape(int(layer2.shape[0]/2), 2), temploss, verbose=False);
+            # explicitely assign the masks because the __init__ function of DeepNet would otherwise set them from zero and randomly 
             net1.setMask(mask2); # set the mask of the first net equals to the mask of the second net (a swap..)
             net2.setMask(mask1); # ..
             net1.setFullyConnected(False); # remember to set that the new net is not fully connected (ok maybe we have to change the __init__ of DeepNet to make it automatic :/ )
@@ -55,11 +56,45 @@ def onePointCrossover(net1, net2, p):
         layer1 = np.append(layer1, np.array([[net2.W[i].shape[1], net2.activations[i]] for i in range(cut_point[1], net2.activations.shape[0])]));
         # define the structure that is used to create the second net's layers as a composition of the first half of the second net plus the first half of the second
         layer2 = np.array([[net2.W[i].shape[1], net2.activations[i]] for i in range(cut_point[1])]);
-        layer2 = np.append(layer2, np.array([[net1.W[i].shape[1], net1.activations[i]] for i in range(cut_point[0], net1.activations.shape[0])]));
-        # create the nets invoking the DeepNet module's __init__   
-        temploss = net1.loss; # this one is used to prevent that the second net has the same loss function as the first one
-        net1 = dn.DeepNet(net1.W[0].shape[0], layer1.reshape(int(layer1.shape[0]/2), 2), net1.loss, False);
-        net2 = dn.DeepNet(net2.W[0].shape[0], layer2.reshape(int(layer2.shape[0]/2), 2), temploss, False); 
+        layer2 = np.append(layer2, np.array([[net1.W[i].shape[1], net1.activations[i]] for i in range(cut_point[0], net1.activations.shape[0])]));  
+        # make a copy of the masks of both the nets
+        if (net1.fully_connected is False) and (net2.fully_connected is False): 
+            mask1 = cp.deepcopy(net1.mask);
+            mask2 = cp.deepcopy(net2.mask);
+            net_conn1 = net1.connection_percentage;
+            net_conn2 = net2.connection_percentage;
+            temploss = net1.loss; # this one is used to prevent that the second net has the same loss function as the first one
+            net1 = dn.DeepNet(net1.W[0].shape[0], layer1.reshape(int(layer1.shape[0]/2), 2), net1.loss, False);
+            net2 = dn.DeepNet(net2.W[0].shape[0], layer2.reshape(int(layer2.shape[0]/2), 2), temploss, False);
+            # assign the masks that we saved
+            net1.mask.append(mask1[:cut_point[0]]);
+            net1.mask.append(mask2[cut_point[1]:]);
+            net2.mask.append(mask2[:cut_point[1]]);
+            net2.mask.append(mask1[cut_point[0]:]);
+            net1.mask  = [val for sublist in net1.mask for val in sublist];
+            net2.mask  = [val for sublist in net2.mask for val in sublist];
+            # make the shapes of the masks coincide (just at the cut points, we select to resize the new net accoring to the minimum size of the two nets)
+            if net1.mask[cut_point[0]-1].shape[1] < net1.mask[cut_point[0]].shape[0]:
+                net1.mask[cut_point[0]] = np.delete(net1.mask[cut_point[0]], net1.mask[cut_point[0]].shape[0]-net1.mask[cut_point[0]-1].shape[1], axis=0);
+            else:
+                net1.mask[cut_point[0]-1] = np.delete(net1.mask[cut_point[0]-1], net1.mask[cut_point[0]-1].shape[1]-net1.mask[cut_point[0]].shape[0], axis=1);
+            if net2.mask[cut_point[1]-1].shape[1] < net2.mask[cut_point[1]].shape[0]:
+                net2.mask[cut_point[1]] = np.delete(net2.mask[cut_point[1]], net2.mask[cut_point[1]].shape[0]-net2.mask[cut_point[1]-1].shape[1], axis=0);
+            else:
+                net2.mask[cut_point[1]-1] = np.delete(net2.mask[cut_point[1]-1], net2.mask[cut_point[1]-1].shape[1]-net2.mask[cut_point[1]].shape[0], axis=1);
+            temploss = net1.loss; # this one is used to prevent that the second net has the same loss function as the first one
+            net1 = dn.DeepNet(net1.W[0].shape[0], layer1.reshape(int(layer1.shape[0]/2), 2), net1.loss, verbose=False);
+            net2 = dn.DeepNet(net2.W[0].shape[0], layer2.reshape(int(layer2.shape[0]/2), 2), temploss, verbose=False); 
+            net1.mask = mask1;
+            net2.mask = mask2;
+            net1.fully_connected = False;
+            net2.fully_connected = False;
+            net1.connection_percentage = net_conn1;
+            net2.connection_percentage = net_conn2;                
+        else:
+            temploss = net1.loss; # this one is used to prevent that the second net has the same loss function as the first one
+            net1 = dn.DeepNet(net1.W[0].shape[0], layer1.reshape(int(layer1.shape[0]/2), 2), net1.loss, False);
+            net2 = dn.DeepNet(net2.W[0].shape[0], layer2.reshape(int(layer2.shape[0]/2), 2), temploss, False); 
     # swap the learning rates between the two nets
     temp = net1.learning_rate;
     net1.learning_rate = net2.learning_rate;
