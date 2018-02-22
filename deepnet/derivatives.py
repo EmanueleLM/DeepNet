sign error in KL fixed
KL(X, Y) = CrossEntropy(X, Y) - H(X)

deepnet/derivatives.py
@@ -49,7 +49,7 @@ def dy_KL(y, t):
     y[y==0] = 1e-10;
     entropy = 1+np.log2(y);
     cross_entropy = (y-t)/(y*(1-y));
-    return entropy + cross_entropy;
+    return cross_entropy - entropy;
 
 # (partial) derivative of sigmoid function wrt variable z
 # the partial is not in the common math sense, but in the sense that the "residual" dZ is not calculated at this step

deepnet/loss.py
@@ -38,4 +38,4 @@ def loss_KL(y, t):
     y[y==0] = 1e-10;
     entropy = y*np.log2(y);
     cross_entropy = -t*np.log2(y)-(1-t)*np.log2(1-y);
-    return entropy + cross_entropy;
+    return cross_entropy - entropy;