0960b564bd18bc9061b5b0f6ecfbe0ea1ed3df03
Iperboreo_ <emanuele.lamalfa@mail.polimi.it>
2018-04-01 23:20:07 +0200

    one pt crossover for strings

--- deepnet/genetic/crossover.py
+++ deepnet/genetic/crossover.py
@@ -44,18 +44,9 @@ def one_point_crossover(net1, net2, p):
     W2 = cp.deepcopy(net2.weights);
     
     # recreate them from scratch and re-assigm them to each net
-    net1.weights = list();
-    net2.weights = list();
-    
-    # assign the weights that we've saved
-    net1.weights.append(W1[:cut_point[0]]);
-    net1.weights.append(W2[cut_point[1]:]);
-    net2.weights.append(W2[:cut_point[1]]);
-    net2.weights.append(W1[cut_point[0]:]);    
-    # flatten the lists
-    net1.weights  = list(chain.from_iterable(net1.weights));
-    net2.weights  = list(chain.from_iterable(net2.weights));
-    
+    net1.weights = W1[:cut_point[0]] + W2[cut_point[1]:];
+    net2.weights = W2[:cut_point[1]] + W1[cut_point[0]:];
+          
     # make the shapes of the weights coincide:
     #   (just at the cut points, we select to resize the new net accoring to 
     #   the minimum size of the two nets)
@@ -98,17 +89,9 @@ def one_point_crossover(net1, net2, p):
         net_conn2 = net2.connection_percentage;
         
         # recreate the masks
-        net1.mask = list();
-        net2.mask = list();        
-        # assign the masks that we saved
-        net1.mask.append(mask1[:cut_point[0]]);
-        net1.mask.append(mask2[cut_point[1]:]);
-        net2.mask.append(mask2[:cut_point[1]]);
-        net2.mask.append(mask1[cut_point[0]:]);
+        net1.mask = mask1[:cut_point[0]] + mask2[cut_point[1]:];
+        net2.mask = mask2[:cut_point[1]] + mask1[cut_point[0]:];        
         
-        # flatten the lists
-        net1.mask  = list(chain.from_iterable(net1.mask));
-        net2.mask  = list(chain.from_iterable(net2.mask));
         # make the shapes of the masks coincide 
         #   (just at the cut points, we select to resize the new net accoring 
         #   to the minimum size of the two nets)
@@ -150,24 +133,52 @@ def one_point_crossover(net1, net2, p):
     net1.learning_rate = net2.learning_rate;
     net2.learning_rate = temp;
     
-    return net1, net2;
+    return [net1, net2];
 
-# TODO:
 # one point crossover with strings that represents each net: takes the two nets
 #  and divide them in two parts each first part is attached to the second part 
 #  of the other net and we obtain two new nets
-# obviously we need to modify (in general) the number of connections of the two
-#    new nets in the point
-# where the original one are splitted
 # takes as input
 #   net1, the first net (DeepNet object)
 #   net2, the second net (DeepNet object)
 #   p, the probability that the crossover happens (this one is usually much 
 #       bigger than the mutation  probability, e.g. 50%)
 # returns
-#   net1, net2, the modified nets after the crossover
+#   net1, net2, the post-crossover nets' encodings
 def one_point_crossover_string(net1, net2, p):
-    pass;
+    
+    # elements' separator in the net's representation
+    sep = ',';
+    
+    # get each element of the nets by splitting the strings that represent them
+    #  in their components
+    el = (net1.split('['), net2.split('['));
+    input_size = ((el[0][0].replace(sep, '')), (el[1][0].replace(sep, ''))) ;
+    
+    # get the layers' description
+    el = (el[0][1].split(']'), el[1][1].split(']'));
+    net_layers = (el[0][0].split(sep), el[1][0].split(sep));
+    
+    # get the loss and learning rate
+    el = (el[0][1].split(sep)[1:], el[1][1].split(sep)[1:]);
+    net_loss = (el[0][0].replace(sep, ''), el[1][0].replace(sep, ''));
+    net_l_rate = (el[0][1].replace(sep, ''), el[1][1].replace(sep, ''));
+    
+    # cut points for the layers: the 2* is due to the <neurons, activations> couples
+    cp1 = 2*np.random.randint(1, int(len(net_layers[0])/2));
+    cp2 = 2*np.random.randint(1, int(len(net_layers[1])/2));
+    
+    # swap the layers according to the cuts
+    l1 = net_layers[0][0:cp1] + net_layers[1][cp2:];
+    l2 = net_layers[1][0:cp2] + net_layers[0][cp1:];
+    # clean the string result
+    net_layers = (str(l1).replace("'", ''), str(l2).replace("'", ''));
+    
+    # rebuild the nets' representations   
+    enc1 = input_size[0] + sep + net_layers[0] + sep + net_loss[0] + sep +net_l_rate[0];
+    enc2 = input_size[1] + sep + net_layers[1] + sep + net_loss[1] + sep +net_l_rate[1];
+
+    return [enc1, enc2];       
 
 # TODO:
 # uniform crossover: takes two nets and mix them uniformely (i.e. each layer of
